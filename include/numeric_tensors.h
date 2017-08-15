6bedecca71ff2f7b86baf63d65534ce2f7dac025
Christopher Kelly <ckelly@phys.columbia.edu>
2017-08-15 11:59:51 -0400

    Added a true generalized tensor class to numeric_tensors

--- include/numeric_tensors.h
+++ include/numeric_tensors.h
@@ -272,6 +272,201 @@ public:
 
 
 
+
+
+//A true general tensor type
+template<typename T,int Rank,int D>
+struct _tensor_helper{
+  static inline size_t vol(const int* sizes){
+    return sizes[0]*_tensor_helper<T,Rank,D-1>::vol(sizes+1);
+  }
+  static inline size_t map(const size_t accum, const int *elem, const int *sizes){
+    return _tensor_helper<T,Rank,D-1>::map( elem[0] + accum*sizes[0], elem+1,sizes+1);
+  }
+  static inline void unmap(int *into, const size_t off, const int *sizes, const size_t vol){
+    size_t subvol = vol/sizes[0];
+    *into = off / subvol;
+    _tensor_helper<T,Rank,D-1>::unmap(into+1, off % subvol, sizes+1, subvol);
+  }
+};
+template<typename T,int Rank>
+struct _tensor_helper<T,Rank,-1>{
+  static inline size_t vol(const int* lst){
+    return 1;
+  }
+  static inline size_t map(const size_t accum, const int *elem, const int *sizes){
+    return accum;
+  }
+  static inline void unmap(int *into, const size_t off, const int *sizes, const size_t vol){};
+};
+
+struct _NTcommonproperties{
+  std::vector<int> sizes;
+  _NTcommonproperties(const std::vector<int> &s): sizes(s){}
+  
+  inline bool operator==(const _NTcommonproperties &r) const{
+    if(sizes.size() != r.sizes.size()) return false;
+    for(int i=0;i<sizes.size();i++) if(sizes.at(i) != r.sizes.at(i)) return false;
+    return true;
+  }
+  inline bool operator!=(const _NTcommonproperties &r) const{
+    return !(*this == r);
+  }
+};
+std::ostream & operator<<(std::ostream &os, const _NTcommonproperties &p){
+  os << "(";
+  for(int i=0;i<p.sizes.size()-1;i++) os << p.sizes.at(i) << ",";
+  os << p.sizes.back() << ")";
+  return os;
+}
+
+template<typename T, int R>
+class NumericTensor;
+
+template<typename T, int R>
+struct _NTprinter{
+  static void print(std::ostream &os, const NumericTensor<T,R> &t);
+};
+
+
+
+
+template<typename DataType, int Rank>
+class NumericTensor{
+  typedef _tensor_helper<DataType,Rank,Rank-1> helper;
+  std::vector<int> dsizes;
+  size_t vol;
+  std::vector<DataType> data;
+
+  inline size_t map(const int *elem) const{ return helper::map(0,elem,dsizes.data()); }
+  inline void unmap(int* into, const size_t off) const{ return helper::unmap(into,off,dsizes.data(),vol); }
+
+  friend struct getElem<NumericTensor<DataType,Rank> >;
+  friend struct _NTprinter<DataType,Rank>;
+
+  template<typename T, int R>
+  friend class NumericTensor;
+public:
+  inline NumericTensor() = default;
+  inline explicit NumericTensor(int const* _size): dsizes(_size,_size+Rank), vol(helper::vol(_size)), data(vol){
+  }
+  inline explicit NumericTensor(const std::vector<int> &_size): NumericTensor(_size.data()){}
+  inline explicit NumericTensor(std::initializer_list<int> _size): NumericTensor(_size.begin()){}
+  inline NumericTensor(const NumericTensor<DataType,Rank> &r) = default;
+  inline NumericTensor(NumericTensor<DataType,Rank> &&r) = default;
+  
+  typedef NumericTensor<DataType,Rank> ET_tag;
+  template<typename U, typename std::enable_if<std::is_same<typename U::ET_tag, ET_tag>::value && !std::is_same<U,ET_tag>::value, int>::type = 0>
+  NumericTensor(U&& expr){
+    std::cout << "ET constructor got common_properties " << expr.common_properties() << std::endl;
+    this->resize(expr.common_properties().sizes);
+    for(size_t i=0;i<this->vol;i++){
+      data[i] = expr[i];
+      std::cout << i << " " << data[i] << std::endl;
+    }
+  }
+
+  inline NumericTensor& operator=(const NumericTensor<DataType,Rank> &r) = default;
+  inline NumericTensor& operator=(NumericTensor<DataType,Rank> &&r) = default;
+  
+   //will invalidate existing values
+  inline void resize(int const* _size){
+    dsizes = std::vector<int>(_size,_size+Rank);
+    vol = helper::vol(_size);
+    data.resize(vol);
+    (std::cout << "Resized to " << vol << std::endl).flush();
+  }
+  inline void resize(const std::vector<int> & _size){ return resize(_size.data()); } 
+  inline void resize(std::initializer_list<int> _size){ return resize(_size.begin()); } 
+  
+  inline const DataType &operator()(std::initializer_list<int> elem) const{
+    return data[map(elem.begin())];
+  }
+  inline DataType &operator()(std::initializer_list<int> elem){
+    return data[map(elem.begin())];
+  }
+  inline const DataType &operator()(int const* elem) const{
+    return data[map(elem)];
+  }
+  inline DataType &operator()(int const* elem){
+    return data[map(elem)];
+  }  
+  
+  inline const std::vector<int> sizes() const{ return dsizes; }
+  inline const int size(const int dir) const{ return dsizes[dir]; }
+
+  template<typename Rule>
+  auto transform(const Rule &rule = Rule()) const -> NumericTensor<typename std::decay<decltype(rule(data[0]))>::type,Rank>{ 
+    NumericTensor<typename std::decay<decltype(rule(data[0]))>::type,Rank> out(dsizes.data());
+    for(size_t i=0;i<vol;i++) out.data[i] = rule(data[i]); //Rule must have NewDataType operator()(const DataType &from)
+    return out;
+  }
+
+  template<typename Rule>
+  NumericTensor<DataType,Rank-1> reduce(const int dim, const Rule &rule = Rule()) const{
+    int new_size[Rank-1];
+    int i=0;
+    for(int ii=0;ii<Rank;ii++) if(ii != dim) new_size[i++] = dsizes[ii];    
+    NumericTensor<DataType,Rank-1> out(new_size);
+    for(size_t i=0;i<out.vol;i++){
+      int oe[Rank-1]; out.unmap(oe,i);
+      rule(out.data[i], oe, *this); //Rule must have operator()(DataType &into, int const* coord, const NumericTensor<DataType,Rank> &from)
+    }
+    return out;
+  }
+  
+};
+
+template<typename T, int R>
+struct getElem<NumericTensor<T,R> >{
+  static inline auto elem(const NumericTensor<T,R> &v, const int i)->decltype(v.data[i]){ return v.data[i]; }
+  static inline auto elem(NumericTensor<T,R> &v, const int i)->decltype(v.data[i]){ return v.data[i]; }
+  static inline _NTcommonproperties common_properties(const NumericTensor<T,R> &v){ return _NTcommonproperties(v.dsizes); }
+};
+
+template<typename T, int R>
+void _NTprinter<T,R>::print(std::ostream &os, const NumericTensor<T,R> &t){
+  int e[R];
+  for(size_t i=0;i<t.vol;i++){
+    t.unmap(e,i);
+    os << '(';
+    for(int d=0;d<R-1;d++) os << e[d] << ',';
+    os << e[R-1] << ") : " << t.data[i] << std::endl;
+  }
+};
+
+//Matrix specialization
+template<typename T>
+struct _NTprinter<T,2>{
+  static void print(std::ostream &os, const NumericTensor<T,2> &t){
+    for(int i=0;i<t.size(0);i++){
+      for(int j=0;j<t.size(1)-1;j++){
+	os << t({i,j}) << ", ";
+      }
+      os << t({i,t.size(1)-1}) << "\n";
+    }
+  }
+};
+//Vector specialization
+template<typename T>
+struct _NTprinter<T,1>{
+  static void print(std::ostream &os, const NumericTensor<T,1> &t){
+    for(int j=0;j<t.size(0)-1;j++){
+      os << t({j}) << ", ";
+    }
+    os << t({t.size(0)-1});
+  }
+};
+
+template<typename T, int R>
+inline std::ostream & operator<<(std::ostream &os, const NumericTensor<T,R> &t){
+  _NTprinter<T,R>::print(os,t);
+  return os;
+}
+
+
+
+
 #include<numeric_tensors_ET.h>
 
 #endif
