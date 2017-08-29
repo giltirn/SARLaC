2d1bf3d41c2b787c3fd2a814b65a844daf454271
Christopher Kelly <ckelly@phys.columbia.edu>
2017-08-29 14:47:27 -0400

    Added an parser for math expressions in text format

---
+++ main/test_expression_parse.C
@@ -0,0 +1,25 @@
+#include<expression_parse.h>
+
+//This is just a basic calculator with assignable symbols!
+int main(const int argc, const char* argv[]){
+  assert(argc >= 2);
+  assert(argc % 2 == 0);
+  std::string s(argv[1]);
+
+  std::vector<std::pair<std::string,double> > symbols;
+  for(int i=2;i<argc;i+=2){
+    std::string sym = argv[i];
+    double val;
+    std::stringstream ss; ss << argv[i+1]; ss >> val;
+    symbols.push_back(std::pair<std::string,double>(sym,val));
+  }
+  
+  expressionAST AST =  mathExpressionParse(s);
+
+  for(int i=0;i<symbols.size();i++){
+    AST.assignSymbol(symbols[i].first,symbols[i].second);
+  }
+
+  std::cout << "Value: " << AST.value() << std::endl;
+  
+}
