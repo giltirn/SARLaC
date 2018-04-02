d0bc98ba7a2ceec362b2ac796cb985db63e6a137
Christopher Kelly <ckelly@phys.columbia.edu>
2018-04-02 15:41:27 -0400

    Generalized sampleSeries to allow for coordinates that are distributions Added sampleSeries ostream output

--- include/data_series/sample_series.h
+++ include/data_series/sample_series.h
@@ -11,21 +11,48 @@
 
 CPSFIT_START_NAMESPACE
 
+template<typename SeriesType, typename SFINAE = void>
+class sampleSeries{};
+
 template<typename SeriesType>
-class sampleSeries{
+class sampleSeries<SeriesType, typename std::enable_if<hasSampleMethod<typename SeriesType::DataType>::value && !hasSampleMethod<typename SeriesType::GeneralizedCoordinate>::value , void>::type >{
 public:
-  typedef typename SeriesType::DataType ElementType; 
   typedef typename SeriesType::GeneralizedCoordinate GeneralizedCoordinate;
-  typedef typename ElementType::DataType DataType;
+  typedef typename SeriesType::DataType::DataType DataType;
 private:
   const int sample;
   SeriesType &series;
 public:
   sampleSeries(SeriesType &_series, const int _sample):series(_series), sample(_sample){ }
   inline int size() const{ return series.size(); }
-  inline typename add_const_if<DataType, SeriesType>::type & value(const int i) const{ return iterate<ElementType>::at(sample, series.value(i)); }
+  inline typename add_const_if<DataType, SeriesType>::type & value(const int i) const{ return iterate<typename SeriesType::DataType>::at(sample, series.value(i)); }
   inline typename add_const_if<GeneralizedCoordinate, SeriesType>::type & coord(const int i) const{ return series.coord(i); }
 };
+
+template<typename SeriesType>
+class sampleSeries<SeriesType, typename std::enable_if<hasSampleMethod<typename SeriesType::DataType>::value && hasSampleMethod<typename SeriesType::GeneralizedCoordinate>::value , void>::type >{
+public:
+  typedef typename SeriesType::GeneralizedCoordinate::DataType GeneralizedCoordinate;
+  typedef typename SeriesType::DataType::DataType DataType;
+private:
+  const int sample;
+  SeriesType &series;
+public:
+  sampleSeries(SeriesType &_series, const int _sample):series(_series), sample(_sample){ }
+  inline int size() const{ return series.size(); }
+  inline typename add_const_if<DataType, SeriesType>::type & value(const int i) const{ return iterate<typename SeriesType::DataType>::at(sample, series.value(i)); }
+  inline typename add_const_if<GeneralizedCoordinate, SeriesType>::type & coord(const int i) const{ return iterate<typename SeriesType::GeneralizedCoordinate>::at(sample, series.coord(i)); }
+};
+
+template<typename SeriesType, typename D> 
+std::ostream & operator<<(std::ostream & stream, const sampleSeries<SeriesType,D> &series){
+  stream << "(";
+  for(int i=0;i<series.size();i++)
+    stream << "{" << series.coord(i) << " " << series.value(i) << "}" << (i != series.size()-1 ? " " : ")");
+  return stream;
+}
+
+
 
 CPSFIT_END_NAMESPACE
 #endif
