5621fbbc207f6bf30aa956ae3858d26a0fa04051
Christopher Kelly <ckelly@phys.columbia.edu>
2018-09-27 11:13:22 -0400

    Added some usage text to latex_table.py

--- scripts/latex_table.py
+++ scripts/latex_table.py
@@ -4,6 +4,31 @@ import os
 import sys
 import re
 
+#--------------------------------------------------------------------------------------------------------------------------------------
+# Automatically generate latex tables calling out to hdf5_print to print parameter entries
+# Usage example: 
+# 1) Import script:   from latex_table import *      (Make sure the directory of this script is in your PYTHONPATH)
+# 2) Generate a 'format array' with table entries:
+#     Basic options include strings such as latex commands, and integers
+#     Special latex commands '\hline' for a horizontal line, '\multicolumn{ncols}{entry}' for multi-column entries are treated appropriately
+#     Interface with hdf5_print is performed using an entry in the form of an array with the first entry the filename, the second the element index 
+#     (for multi-dimensional arrays use a string with comma-separated indices), and finally a hash with any extra commands passed to the program (use {} for default)
+#     Extra commands are the usual command line args but without the leading -, eg "pub_rounding"
+# 3) Call 'tabulate' with the first argument the number of columns, the second the format array and finally a string with the column layout
+
+# Example:
+
+# from latex_table import * 
+
+# fmt = [ '$M_0$',["params.hdf5", 6, {"pub_rounding":-2}],
+#         '$M_1$',["params.hdf5", 7, {"pub_rounding":-2}],
+#         '$\chi^2$',["chisq.hdf5",0,{}],
+#         r'$\chi^2/{\rm dof}$',["chisq_per_dof.hdf5",0,{}]
+# ]
+
+# tabulate(2,fmt,colstr=r'c|c')
+#--------------------------------------------------------------------------------------------------------------------------------------
+
 def hdf5_print(filename, **kwargs):
     arg_str = ""
     for a in kwargs.keys():
